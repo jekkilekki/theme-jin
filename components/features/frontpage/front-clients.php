30b89e1a1d001d366090e73024aacdcb5041c3a7
Aaron Snowberger <jekkilekki@gmail.com>
2016-05-24 11:16:27 +0900

    Update Frontpage clients links

--- components/features/frontpage/front-clients.php
+++ components/features/frontpage/front-clients.php
@@ -2,7 +2,7 @@
 
 ?>
         <li class="clear small-4 medium-3 large-2 columns">
-            <a class="clients-link" href="<?php get_permalink(); ?>" title="See all Projects for <?php get_the_title(); ?>">
+            <a class="clients-link" href="<?php echo get_permalink(); ?>" title="See all Projects for <?php echo get_the_title(); ?>">
                 <figure class="client-figure">
                     <?php if ( has_post_thumbnail() ) the_post_thumbnail( 'medium', array( 'class' => 'desaturate' ) ); ?>
                 </figure>
